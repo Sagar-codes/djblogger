a89e983e911c9fb56ab41617f6c41cbf2d81ccbf
Sagar-codes <paulsagar5433@gmail.com>
2023-05-22 01:23:16 +0530

    added py test and cov and factory and factory boy

--- djblogger/settings/config.py
+++ djblogger/settings/config.py
@@ -29,6 +29,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # application
+    'djblogger.blog'
 ]
 
 MIDDLEWARE = [
--- djblogger/tests/test_example.py
+++ djblogger/tests/test_example.py
@@ -2,4 +2,4 @@ def test_example():
     assert 1 == 1
 
 def test_example1():
-    assert 1 == 2
+    assert 1 == 1
