ba754865b6e0aa82a724284ca99ceb8d17a1182c
Sagar-codes <paulsagar5433@gmail.com>
2023-05-22 00:18:43 +0530

    django blog website using HTMX

---
+++ djblogger/settings/local.py
@@ -0,0 +1,12 @@
+from .config import *
+
+
+"""Generate Secret Key in Shell
+
+python manage.py shell:
+    from django.core.management.utils import get_random_secret_key 
+    print(get_random_secret_key())
+
+Return: @tbl91^@2s=p#7lxc8e$r22lm!j@k7@*#741b!$+ot-_(w!_x7
+
+"""
